bab4e4d07332bb8085044dca96b27c03a024d0fa
Davydova <davydovan06@gmail.com>
2018-07-26 18:32:02 +0300

    Add files via upload

---
+++ Lesson2/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String a, b, c;
+            a = "\nмоя строка 1";
+            b = "\tмоя строка 2";
+            c = "\aмоя строка 3";
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.Write(c);
+
+           
+
+            Console.ReadKey(); 
+        }
+    }
+}
---
+++ Lesson2/ConsoleAppLesson2/ConsoleAppLesson2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppLesson2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Double a = 3.141592653;
+           Decimal b = 2.7182818284590452m;
+
+            Console.WriteLine(a);
+            Console.Write(b);
+
+            Console.ReadKey();
+        }
+    }
+}
